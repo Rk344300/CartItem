cb2d253954d096ec74904860d1a9875118d09066
Rk344300 <rk344300@gmail.com>
2023-01-14 14:04:11 +0530

    perform CRUD opertion with firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 class App extends React.Component {
   constructor(){
@@ -10,10 +11,12 @@ class App extends React.Component {
         products :[],
         loading : true
     }
+    // this.db = firebase.firestore();
 }
  componentDidMount () {
     firebase
      .firestore()
+    //this.db
      .collection('products')
      .onSnapshot((snapshot) =>{
       //console.log(snapshot);
@@ -63,10 +66,23 @@ handleIncreaseQuantity = (product) =>{
       const {products} = this.state;
       const index = products.indexOf(product);
 
-      products[index].qty += 1;
+      // products[index].qty += 1;
 
-      this.setState({
-          products :products
+      // this.setState({
+      //     products :products
+      // })
+
+      const docRef = firebase.firestore().collection('products').doc(products[index].id);
+
+      docRef
+      .update({
+        qty:products[index].qty + 1
+      })
+      .then(() =>{
+        console.log("update successfully");
+      })
+      .catch((error) =>{
+        console.log('error',error);
       })
 
 }
@@ -74,21 +90,48 @@ handleDecreaseQuantity =(product) => {
  console.log('we are in handledec');
  const {products} =this.state;
  const index = products.indexOf(product);
-(products[index].qty !=0) ? products[index].qty -=1 : products[index].qty =0;
 
-    this.setState({
-        products:products
-    })
+ const docRef = firebase.firestore().collection('products').doc(products[index].id);
+
+ docRef
+ .update({
+   qty:products[index].qty > 0 ? products[index].qty - 1 : 0
+ })
+ .then(() =>{
+   console.log("update successfully");
+ })
+ .catch((error) =>{
+   console.log('error',error);
+ })
+
+// (products[index].qty !=0) ? products[index].qty -=1 : products[index].qty =0;
+
+//     this.setState({
+//         products:products
+//     })
 
 }
 
 handleDelete =(id) =>{
     const {products} = this.state;
-    const items = products.filter((item) =>item.id !== id);
+    // const items = products.filter((item) =>item.id !== id);
 
-    this.setState({
-        products : items
-    })
+    // this.setState({
+    //     products : items
+    // })
+
+
+    const docRef = firebase.firestore().collection('products').doc(id);
+
+    docRef
+      .delete()
+      .then(() =>{
+        console.log("deleted successfully");
+      })
+      .catch((error) =>{
+        console.log('error',error);
+      })
+
 }
 
 
@@ -115,6 +158,24 @@ handleDelete =(id) =>{
     return totalPrice;
   }
 
+  addProduct () {
+      firebase
+      .firestore()
+      // this.db
+        .collection('products')
+        .add({
+          img:'',
+          price:400,
+          qty:2,
+          title:'washing machine'
+        })
+        .then((docRef) =>{
+          console.log("product added",docRef);
+        })
+        .catch((error) =>{
+          console.log(error);
+        })
+  }
 
 
   render(){
@@ -122,6 +183,7 @@ handleDelete =(id) =>{
     return (
       <div className="App">
       <Navbar count ={this.getCartCount()} />
+      <button onClick={this.addProduct} style ={{padding:20,fontSize:20}}>Add a Product</button>
         <Cart
            products = {products}
            onInceaseQuantity = {this.handleIncreaseQuantity}
